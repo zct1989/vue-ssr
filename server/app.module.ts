db2e4e2a82cb1bdb6cd8e69e033b594c36f6c2b8
zhuchentong <2037630@163.com>
2021-10-30 12:38:11 +0800

    fixed prod mode

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,21 +1,10 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { createSsrServer } from 'vite-ssr/dev';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SSRMiddleware } from './ssr.middlewave';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  // async configure(consumer: MiddlewareConsumer) {
-  //   const viteServer = await createSsrServer({
-  //     server: {
-  //       middlewareMode: 'ssr',
-  //     },
-  //   });
-  //   consumer.apply(viteServer.middlewares).forRoutes(AppController);
-  // }
-}
+export class AppModule {}
--- server/main.ts
+++ server/main.ts
@@ -2,69 +2,80 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { createSsrServer } from 'vite-ssr/dev';
 import { AppModule } from './app.module';
-import * as Express from 'express';
-import { resolve } from 'path';
-import serveStatic from 'serve-static';
+import * as express from 'express';
+import { resolve, join } from 'path';
+
+// 当前环境类型
 const isProd = process.env.NODE_ENV === 'production';
-const distDir = resolve(`../dist`);
-async function bootstrap() {
-  const express = Express();
-  console.log(process.cwd());
+// 生产环境目录
+const distDir = resolve(process.cwd(), 'dist');
+
+// 创建开发服务
+async function createDevServer(server) {
+  const viteServer = await createSsrServer({
+    server: {
+      middlewareMode: 'ssr',
+    },
+  });
+
+  // 响应前端请求
+  server.use(/^(?!\/?(api|doc|graphql)).+$/, viteServer.middlewares);
+}
+
+// 创建生产服务
+async function createProdServer(server) {
+  const clientPath = resolve(distDir, 'client');
+  const serverPath = resolve(distDir, 'server');
+
+  // 获取Manifest配置
+  const manifest = await import(join(clientPath, 'ssr-manifest.json'));
+  // 获取Render函数
+  const { default: render } = await import(serverPath);
+  // 安装资源文件
+  server.use(
+    express.static(clientPath, {
+      index: false,
+    }),
+  );
+
+  // 响应前端请求
+  server.get(/^(?!\/?(api|graphql)).+$/, async (request, response) => {
+    // 请求路径
+    const url =
+      request.protocol + '://' + request.get('host') + request.originalUrl;
+    // 渲染文件
+    const { html, status, statusText, headers } = await render(url, {
+      manifest,
+      preload: true,
+      request,
+      response,
+    });
+
+    response.writeHead(status || 200, statusText || headers, headers);
+    response.end(html);
+  });
+}
+
+function setupExpressServer() {
+  const server = express();
 
   if (isProd) {
-    const viteServer = await createSsrServer({
-      root: process.cwd(),
-      server: {
-        middlewareMode: 'ssr',
-      },
-    });
-
-    express.get(/^(?!\/?(api|doc|graphql)).+$/, viteServer.middlewares);
+    createProdServer(server);
   } else {
-    const { ssr } = await import(resolve(distDir, 'server', 'package.json'));
-    const manifest = await import(
-      resolve(distDir, 'client', 'ssr-manifest.json')
-    );
-    const { default: renderPage } = import(resolve(distDir, 'server')) as any;
-    for (const asset of ssr.assets || []) {
-      express.use('/' + asset, express.static(resolve(`client` + asset)));
-    }
-
-    express.use(require('compression')());
-    express.use(
-      serveStatic(toAbsolute(prodPath), {
-        index: false,
-      }),
-    );
-
-    express.get(/^(?!\/?(api|doc|graphql)).+$/, viteServer.middlewares);
-    server.get('*', async (request, response) => {
-      const url =
-        request.protocol + '://' + request.get('host') + request.originalUrl;
-
-      const { html, status, statusText, headers } = await renderPage(url, {
-        manifest,
-        preload: true,
-        // Anything passed here will be available in the main hook
-        request,
-        response,
-        // initialState: { ... } // <- This would also be available
-      });
-
-      response.writeHead(status || 200, statusText || headers, headers);
-      response.end(html);
-    });
-
-    // // The manifest is required for preloading assets
-    // const manifest = import(`}/client/ssr-manifest.json`);
-
-    // // This is the server renderer we just built
-    // const { default: renderPage } = import(`${dist}/server`);
+    createDevServer(server);
   }
 
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(express));
+  return server;
+}
+
+async function bootstrap() {
+  const server = await setupExpressServer();
+
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   await app.listen(3000);
 }
 
 bootstrap();
+
+console.log('123123');
